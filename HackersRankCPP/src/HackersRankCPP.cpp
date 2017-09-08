c1e34e7fdad218c36fe95f0ebe50b40a67489407
esmiejose <esmie_jose@my.uri.edu>
2017-09-08 12:59:29 -0400

    Workspace - Hello World

---
+++ HackersRankCPP/src/HackersRankCPP.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HackersRankCPP.cpp
+// Author      : Esmie Jose
+// Version     :
+// Copyright   : @EsmieJose2017 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
